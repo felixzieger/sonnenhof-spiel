929febacb30c71f21c454b42aba440faa85d5f47
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-09-19 20:00:56 +0200

    react router v7

---
+++ src/root.tsx
@@ -0,0 +1,50 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Rette den Sonnenhof</title>
+        <meta name="description" content="Hilf uns die Tiere auf dem Sonnenhof einzufangen!" />
+        <meta name="author" content="Johanna und Felix" />
+        <meta property="og:image" content="/og-image.png" />
+        <script defer data-domain="spiel.sonnenhof-zieger.de" src="https://plausible.sonnenhof-zieger.de/js/script.js"></script>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            {children}
+            <ScrollRestoration />
+            <Scripts />
+          </TooltipProvider>
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
+
+export default function Root() {
+  return <Outlet />;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { reactRouter } from "@react-router/dev/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react()
+    reactRouter()
   ].filter(Boolean),
   resolve: {
     alias: {
